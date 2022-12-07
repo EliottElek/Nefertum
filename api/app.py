4ad6cc97410f50fd9f3009b04e10e29514c79010
Eliott Morcillo <eliott.morcillo@gmail.com>
2022-12-07 10:32:15 +0100

    feat: new themes

--- api/app.py
+++ api/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, session, request
+from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import json
 import random
+from getLikelyResults import getLikelyResults
 
 
 app = Flask(__name__)
@@ -106,10 +107,16 @@ class Start(Resource):
         return {'data': getNextQuestion()}, 200
 
 
+class LikelyResults(Resource):
+    def get(self, session_id):
+        return {'data': getLikelyResults()}, 200
+
+
 api.add_resource(Questions, '/questions')
 api.add_resource(Start, '/start')
 api.add_resource(Answer, '/answer/<session_id>')
 api.add_resource(Answers, '/answers/<session_id>')
+api.add_resource(LikelyResults, '/likely_results/<session_id>')
 
 if __name__ == '__main__':
     app.run()  # run our Flask app

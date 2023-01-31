ddb362d262a16a60a0049d1ec71dfb9110363451
Eliott Morcillo <eliott.morcillo@gmail.com>
2023-01-31 23:06:10 +0100

    feat: download button

--- client/pages/added-sources.js
+++ client/pages/added-sources.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Typography } from "@material-tailwind/react";
+import { Button, Typography } from "@material-tailwind/react";
 import { supabase } from "../lib/supabase";
 import { Table } from "flowbite-react";
 import toast from "react-hot-toast";
 import Head from "next/head";
 import { CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { tableToCSV } from "../lib/toCsv";
 const AddedSources = () => {
   const [sources, setSources] = useState(null);
   const [attributes, setAttributes] = useState(null);
@@ -38,10 +39,15 @@ const AddedSources = () => {
         <title>Nefertum | Added sources</title>
         <meta name="description" content="Nefertum, akinator for smells." />
       </Head>
-      <div className="w-8xl max-w-[90vw] overflow-auto">
+      <div className="flex pt-4 items-center justify-between w-full">
         <Typography variant="h3" className="text-gray-50 my-2">
           Community added sources
         </Typography>
+        <Button onClick={() => tableToCSV()} color="purple">
+          Download
+        </Button>
+      </div>
+      <div className="w-8xl max-w-[90vw] h-[70vh] overflow-auto">
         {sources?.length === 0 ? (
           <p>No source for the moment.</p>
         ) : (
@@ -69,11 +75,10 @@ const AddedSources = () => {
                           (m) =>
                             m.source_id === source.id &&
                             m.attribute_id === att.id
-                        ) !== -1 ? (
-                          <CheckIcon className="w-5 h-5 text-green-500" />
-                        ) : (
-                          <XMarkIcon className="w-5 h-5 text-red-500" />
-                        )}
+                        ) !== -1
+                          ? "1" // <CheckIcon className="w-5 h-5 text-green-500" />
+                          : "0" // <XMarkIcon className="w-5 h-5 text-red-500" />
+                        }
                       </Table.Cell>
                     ))}
                   </Table.Row>
